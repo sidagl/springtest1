29fc9ccd1727b7e9a09c40e4a60a863249d5abe8
yirez <irezyigit@gmail.com>
2021-05-15 11:42:37 +0300

    changed fields to properties, fiddling around with spring in general

--- src/main/java/com/springtests/yigit/test1/Alien.java
+++ src/main/java/com/springtests/yigit/test1/Alien.java
@@ -6,19 +6,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class Alien {
 
+
+    private int age=15;
+
     // this allows autowiring of property to controlled component and allows usage without init
     @Autowired
-    Laptop laptop;
+    private Laptop laptop;
+
+    public Alien(){
+        System.out.println("Alien obj created");
+    }
 
     public void code() {
         System.out.println("Test1");
     }
 
     public void probe() {
-
+        System.out.println("You've been probed");
     }
 
     public void control() {
         laptop.compile();
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
     }
 }
--- src/main/java/com/springtests/yigit/test1/Test1Application.java
+++ src/main/java/com/springtests/yigit/test1/Test1Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
@@ -12,20 +13,31 @@ public class Test1Application {
 
     public static void main(String[] args) {
         //Getting bean from app context
-        ApplicationContext appContext = SpringApplication.run(Test1Application.class, args);
+        //ApplicationContext appContext = SpringApplication.run(Test1Application.class, args);
         //inject Alien class using @Component annotation -
         // not responsible for object lifecycle. DI
-        Alien obj = appContext.getBean(Alien.class);
-        obj.code();
-        obj.control();
+        //Alien obj = appContext.getBean(Alien.class);
+        //obj.code();
+        //obj.control();
 
-        System.out.println("*******");
+        //System.out.println("*******");
 
 
         //Getting bean from BeanFactory
-        // -- depends on spring xml config
-        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        // -- depends on spring.xml config
+        // Spring Container returns only 1 object by default when you add it to spring xml
+        // BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml")); //deprecated
+        ApplicationContext beanFactory = new ClassPathXmlApplicationContext("spring.xml");
+
         Alien alien = (Alien) beanFactory.getBean("alien");
+        alien.setAge(4234);
         alien.code();
+        alien.control();
+        System.out.println("alien age:" + alien.getAge());
+
+
+        Alien alien2 = (Alien) beanFactory.getBean("alien");
+        alien2.code();
+        System.out.println("alien age:" + alien2.getAge());
     }
 }
